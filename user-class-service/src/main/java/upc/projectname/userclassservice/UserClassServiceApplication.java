3f47c0557367f68b875787a9c0a47ee589c46021
huanghe <h3145267275@163.com>
2025-01-24 17:19:08 +0800

    创建微服务模块-user-class-service1

---
+++ user-class-service/src/main/java/upc/projectname/userclassservice/UserClassServiceApplication.java
@@ -0,0 +1,13 @@
+package upc.projectname.userclassservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserClassServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserClassServiceApplication.class, args);
+    }
+
+}
